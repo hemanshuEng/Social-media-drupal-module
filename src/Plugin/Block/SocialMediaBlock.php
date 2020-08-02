13fcd9fb02bdfa97c3aaa1e5d8dd933108528363
Hemanshu Khodiyar <hemanshu.eng@gmail.com>
2020-08-02 23:59:30 +0100

    module setup

---
+++ src/Plugin/Block/SocialMediaBlock.php
@@ -0,0 +1,69 @@
+<?php
+
+namespace Drupal\champions_social\Plugin\Block;
+
+use Drupal\Core\Block\Annotation\Block;
+use Drupal\Core\Block\BlockBase;
+use Drupal\Core\Form\FormStateInterface;
+
+/**
+ * Provides a 'SocialMediaBlock' block.
+ *
+ * @Block(
+ *  id = "social_media_block",
+ *  admin_label = @Translation("Social Media Block"),
+ * )
+ */
+class SocialMediaBlock extends BlockBase {
+
+  /**
+   * {@inheritdoc}
+   */
+  public function defaultConfiguration() {
+    return [
+    ] + parent::defaultConfiguration();
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public function blockForm($form, FormStateInterface $form_state) {
+
+    $form = parent::blockForm($form, $form_state);
+    $config = $this->getConfiguration();
+    $form['drupal_boilerplate_title'] = [
+      '#type' => 'textfield',
+      '#title' => $this->t('Title'),
+      '#default_value' => isset($config['drupal_boilerplate_title']) ? $config['drupal_boilerplate_title'] : '',
+    ];
+    return $form;
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public function blockSubmit($form, FormStateInterface $form_state) {
+    $this->configuration['drupal_boilerplate_title'] = $form_state->getValue('drupal_boilerplate_title');
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public function build() {
+    $config = $this->getConfiguration();
+    $build = [];
+    $build['#theme'] = 'champions_social';
+    $build['#conten'] = !empty($config['drupal_boilerplate_title']) ? $config['drupal_boilerplate_title'] : '';
+
+    return $build;
+  }
+//  /**
+//   * {@inheritdoc}
+//   */
+//  public function build() {
+//    return array(
+//      '#markup' => $this->t('Hello, World!'),
+//    );
+//  }
+
+}
